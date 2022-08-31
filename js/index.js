b3e57068528db09da7988e5fe5430af879f17706
joyharris78 <106271016+joyharris78@users.noreply.github.com>
2022-08-31 15:42:03 -0400

    changes for form validation -still working

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-const taskManager = new TaskManager();
-console.log(taskManager);
+//const taskManager = new TaskManager();
+//console.log(taskManager);
 
+const form = document.querySelector('#form');
+const submitBtn = document.querySelector('#submitBtn');
 const taskName = document.querySelector('#taskName');
 const taskDescription = document.querySelector('#taskDescription');
 const dueDate = document.querySelector('#dueDate');
@@ -8,23 +10,26 @@ const taskPriority = document.querySelector('#taskPriority');
 const assignedTo = document.querySelector('#assignedTo');
 
 
-form.addEventListener('submit', (e) => {
- e.preventDefault();
 
- validFormFieldInput();
-});
 
 let validFormFieldInput = ()=> {
     //gets values from the form inputs
    const taskNameValue = taskName.value.trim();
    const taskDescriptionValue = taskDescription.value.trim();
    const dueDateValue = dueDate.value;
-   const taskPrioityValue = taskPriority.value;
+   const taskPriorityValue = taskPriority.value;
    const assignedToValue = assignedTo.value;
    
-   if(taskNameValue === ''){
-       //error message
+
+   if(taskNameValue === '' || taskDescriptionValue === '' || dueDateValue === '' || taskPriorityValue === '' || assignedToValue === ''){
+       alert('Please complete form')
    } else{
-       // success message
+      alert('Form has been submitted')
    }
 }
+
+form.addEventListener('submit', (e) => {
+    //e.preventDefault();
+   
+    validFormFieldInput();
+   });
